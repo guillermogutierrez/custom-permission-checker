75c312cc79e72f6d376a798a603def6744e710ee
Guillermo Gutierrez <guillermo.gutierrez@liferay.com>
2017-04-19 10:34:17 +0200

    Added group service usage

--- src/main/java/com/liferay/portal/security/permission/custom/CustomPermissionChecker.java
+++ src/main/java/com/liferay/portal/security/permission/custom/CustomPermissionChecker.java
@@ -1,18 +1,46 @@
 package com.liferay.portal.security.permission.custom;
 
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.service.GroupLocalServiceUtil;
 import com.liferay.portal.security.permission.AdvancedPermissionChecker;
 
 public class CustomPermissionChecker extends AdvancedPermissionChecker {
 
-	@Override
-	public CustomPermissionChecker clone() {
-		System.out.println("Created a clone using my custom permission checker implementation!!!");
-		return new CustomPermissionChecker();
-	}
-	
-	@Override
-	public boolean hasPermission(long groupId, String name, String primKey, String actionId) {
-		System.out.println("Using my custom permission checker implementation!!!");
-		return super.hasPermission(groupId, name, primKey, actionId);
-	}
+    @Override
+    public CustomPermissionChecker clone() {
+
+        System.out
+            .println(
+                "Created a clone using my custom permission checker implementation!!!");
+        return new CustomPermissionChecker();
+    }
+
+    @Override
+    public boolean hasPermission(
+        long groupId, String name, String primKey, String actionId) {
+
+        try {
+            Group guestGroup = GroupLocalServiceUtil
+                .getGroup(groupId);
+
+            System.out
+                .println(
+                    "Using my custom permission checker implementation for the group " +
+                        groupId +
+                        guestGroup
+                            .getFriendlyURL());
+        }
+        catch (PortalException e) {
+            // TODO Auto-generated catch block
+            System.out
+                .println(
+                    "Error using my custom permission checker implementation!!!" +
+                        e
+                        .getMessage());
+        }
+        System.out
+            .println("Using my custom permission checker implementation!!!");
+        return super.hasPermission(groupId, name, primKey, actionId);
+    }
 }
